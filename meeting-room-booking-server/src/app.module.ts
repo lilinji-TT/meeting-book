137e14d82472bf15b500aac666de4ebfed32e084
李林积 <112566451+lilinji-TT@users.noreply.github.com>
2023-11-15 21:23:18 +0800

    user role permission table create

--- meeting-room-booking-server/src/app.module.ts
+++ meeting-room-booking-server/src/app.module.ts
@@ -1,9 +1,32 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { Permission } from './user/entities/permission.entity';
+import { Role } from './user/entities/role.entity';
+import { User } from './user/entities/user.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'Lilinji',
+      database: 'meeting_room_booking_system',
+      synchronize: true,
+      logging: true,
+      entities: [User, Role, Permission],
+      poolSize: 10,
+      connectorPackage: 'mysql2',
+      extra: {
+        authPlugin: 'sha256_password',
+      },
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
